d038e59a9a580a6ddab9113f0ff2c854cbe37af9
Hernandez <ht21002ts@esfe.agape.edu.sv>
2022-11-30 10:43:52 -0600

    proyecto

---
+++ VideoKids/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaVideos.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+    }
+}
